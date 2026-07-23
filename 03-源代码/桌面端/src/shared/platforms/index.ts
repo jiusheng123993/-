716f1acbaa1ed0c0a7a4188aeeb8de483c7a8dab
jiusheng <jiusheng123993@github.com>
2026-07-23 19:51:11 +0800

    chore: 删除桌面端旧方向代码+根目录旧配置
    - 删除 03-源代码/桌面端 (android+electron+capacitor)
    - 删除根目录旧配置 (eslint/vite/tsconfig/electron)
    - 删除 .env.example/electron-builder/CI
    - 删除 10-归档 旧物

--- 03-源代码/桌面端/src/shared/platforms/index.ts
+++
@@ -1,8 +0,0 @@
-export { PlatformContext, usePlatform } from './usePlatform'
-export { getPlatformInfo, resetPlatformCache } from './platformDetect'
-export { AdaptiveModal } from './AdaptiveModal'
-export { FullScreenModal } from './FullScreenModal'
-export { AdaptiveSidebar } from './AdaptiveSidebar'
-export { useLongPress } from './useLongPress'
-export { useAdaptiveTooltip } from './useAdaptiveTooltip'
-export type { Platform, PlatformInfo, DeviceCategory } from './types'
